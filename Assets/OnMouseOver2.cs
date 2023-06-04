dd856ebbc4a0dfeb06ba1733c282c5e0df3664a6
lugggas <lukas.zenner94@googlemail.com>
2023-06-04 17:27:15 +0200

    --wip-- [skip ci]

--- Assets/OnMouseOver2.cs
+++ Assets/OnMouseOver2.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnMouseOver2 : MonoBehaviour
-{
+public class OnMouseOver2 : MonoBehaviour {
     Color m_ColorMultiplier = new Vector4(0.3f, 0.3f, 0.3f, 1f);
     Texture2D m_OriginalTexture;
     SpriteRenderer m_Renderer;
     PolygonCollider2D m_Collider;
 
-    void Start()
-    {
+    void Start() {
         m_Renderer = GetComponent<SpriteRenderer>();
         m_Collider = GetComponent<PolygonCollider2D>();
         m_OriginalTexture = new Texture2D(m_Renderer.sprite.texture.width, m_Renderer.sprite.texture.height);
@@ -18,34 +16,28 @@ public class OnMouseOver2 : MonoBehaviour
         m_OriginalTexture.Apply();
     }
 
-    void OnMouseEnter()
-    {
-        var spriteTexture = m_Renderer.sprite.texture;
-        var spriteX = m_Renderer.sprite.texture.width;
-        var spriteY = m_Renderer.sprite.texture.height;
-        Debug.Log($"height: {spriteY}, width: {spriteX}");
-        for (var x = 0; x < spriteX; x++) {
-            for (var y = 0; y < spriteY; y++) {
-                var pixelsPerUnit = m_Renderer.sprite.pixelsPerUnit;
-                var localPosition2D = new Vector2(m_Renderer.transform.localPosition.x, m_Renderer.transform.localPosition.y);
-                Debug.Log($"localPosition2D: {localPosition2D}");
-                var pixelPosition = new Vector2(x / pixelsPerUnit, y / pixelsPerUnit);
-                Debug.Log($"pixelPosition: {pixelPosition}");
-                var localPosition = localPosition2D + pixelPosition;
-                var closestPoint = m_Collider.ClosestPoint(localPosition);
-                if (closestPoint == localPosition) {
+    void OnMouseEnter() {
+        var sprite = m_Renderer.sprite;
+        var spriteTexture = sprite.texture;
+        var spriteRect = new IntRect(sprite.rect);
+        var offsetX = spriteRect.x;
+        var offsetY = spriteRect.y;
+        var pixelsPerUnit = sprite.pixelsPerUnit;
+        var localPosition = new Vector2(m_Renderer.transform.localPosition.x, m_Renderer.transform.localPosition.y);
+        for (var x = offsetX; x < offsetX + spriteRect.width; x++) {
+            localPosition.x = (x - offsetX) / pixelsPerUnit;
+            for (var y = offsetY; y < offsetY + spriteRect.height; y++) {
+                localPosition.y = (y - offsetY) / pixelsPerUnit;
+                if (m_Collider.OverlapPoint(localPosition)) {
                     var highlightColor = ClampColor(spriteTexture.GetPixel(x, y) * m_ColorMultiplier);
                     spriteTexture.SetPixel(x, y, Color.red);
-                    Debug.Log($"spriteTexture.SetPixel({x}, {y}, {highlightColor})");
                 }
-                spriteTexture.SetPixel(x, y, Color.red);
             }
         }
         spriteTexture.Apply();
     }
 
-    void OnMouseExit()
-    {
+    void OnMouseExit() {
         m_Renderer.sprite.texture.SetPixels(m_OriginalTexture.GetPixels());
         m_Renderer.sprite.texture.Apply();
     }
@@ -53,5 +45,18 @@ public class OnMouseOver2 : MonoBehaviour
     private Color ClampColor(Vector4 rgbaColor) {
         var minColor = new Vector4(1f, 1f, 1f, 1f);
         return Vector4.Min(minColor, rgbaColor);
+    }
+
+    private struct IntRect {
+        public IntRect(Rect rect) {
+            x = (int) rect.x;
+            y = (int) rect.y;
+            width = (int) rect.width;
+            height = (int) rect.height;
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
     }
 }
